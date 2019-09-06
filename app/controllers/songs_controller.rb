30fd63230b3d361472a7ffb78a196028e8627c17
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-06 14:53:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -30,7 +30,7 @@ class SongsController < ApplicationController
   private
   
   def song_params
-    params.require(:genre).permit(:name, :artist_id, :genre_id)
+    params.require(:song).permit(:name, :artist_id, :genre_id)
   end
   
   
