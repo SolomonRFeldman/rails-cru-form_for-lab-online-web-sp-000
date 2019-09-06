9f1c5c3fb0ead97dd9321b62ab39aca249d6d65f
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-06 14:36:38 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -9,15 +9,18 @@ class GenresController < ApplicationController
   end
   
   def create
-    
+    genre = Genre.create(genre_params)
+    redirect_to artist_path(artist)
   end
   
   def edit
-    
+    @genre = Genre.find(params[:id])
   end
   
   def update
-    
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to artist_path(artist)
   end
   
 end
