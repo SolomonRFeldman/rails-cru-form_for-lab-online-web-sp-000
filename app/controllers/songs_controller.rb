9f9481b0cde116782e280fe4bb702958c51fe95b
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-06 14:51:51 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,15 +13,25 @@ class SongsController < ApplicationController
   end
   
   def create
-    
+    song = Song.create(song_params)
+    redirect_to song_path(song)
   end
   
   def edit
-    
+    @song = Song.find(params[:id])
   end
   
   def update
-    
+    song = Song.find(params[:id])
+    song.update(song_params)
+    redirect_to song_path(song)
   end
+  
+  private
+  
+  def song_params
+    params.require(:genre).permit(:name, :artist_id, :genre_id)
+  end
+  
   
 end
